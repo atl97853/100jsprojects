5f0947b2552408d6ed209bbbeb395396f52eba36
atl97853 <128220069+atl97853@users.noreply.github.com>
2024-03-09 17:19:13 -0600

    Add character images

--- 02_pixel_art_game/overworld.js
+++ 02_pixel_art_game/overworld.js
@@ -14,5 +14,39 @@ class Overworld {
 
         }
         image.src = './asset_game/maps/DemoLower.png'
+
+        const x = 5
+        const y = 6
+
+        const shadow = new Image()
+        shadow.onload = () => {
+            this.ctx.drawImage(
+                shadow,
+                0, //left cut
+                0, //top cut
+                32, //width of cut
+                32, //height of cut
+                x * 16 - 8,
+                y * 16 - 18,
+                32,
+                32,
+            )
+        }
+        shadow.src = './asset_game/shadow.png'
+        const hero = new Image()
+        hero.onload = () => {
+            this.ctx.drawImage(
+                hero,
+                0, //left cut
+                0, //top cut
+                32, //width of cut
+                32, //height of cut
+                x * 16 - 8,
+                y * 16 - 18,
+                32,
+                32,
+            )
+        }
+        hero.src = './asset_game/people/hero.png'
     }
 }
