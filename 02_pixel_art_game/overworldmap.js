4b7f563c53c3ec554ad0ddb6a0da3f5b8cba37d2
atl97853 <128220069+atl97853@users.noreply.github.com>
2024-03-11 17:34:32 -0600

    Refactor class

--- 02_pixel_art_game/overworldmap.js
+++ 02_pixel_art_game/overworldmap.js
@@ -1,5 +1,54 @@
 class OverworldMap {
-    constructor() {
+    constructor(config) {
+        this.gameObjects = config.gameObjects
 
+        this.lowerImage = new Image()
+        this.lowerImage.src = config.lowerSrc
+
+        this.upperImage = new Image()
+        this.upperImage.src = config.upperSrc
     }
+
+    drawLowerImage(ctx) {
+        ctx.drawImage(this.lowerImage, 0, 0)
+    }
+
+    drawUpperImage(ctx) {
+        ctx.drawImage(this.upperImage, 0, 0)
+    }
+}
+
+window.OverworldMaps = {
+    DemoRoom: {
+        lowerSrc: './asset_game/maps/DemoLower.png',
+        upperSrcSrc: './asset_game/maps/DemoUpper.png',
+        gameObjects: {
+            hero: new GameObject({
+                x: 5,
+                y: 6,
+            }),
+            npc1: new GameObject({
+                x: 7,
+                y: 9,
+            }),
+        }
+    },
+    Kitchen: {
+        lowerSrc: './asset_game/maps/KitchenLower.png',
+        upperSrcSrc: './asset_game/maps/KitchenUpper.png',
+        gameObjects: {
+            hero: new GameObject({
+                x: 3,
+                y: 1,
+            }),
+            npc1: new GameObject({
+                x: 9,
+                y: 2,
+            }),
+            npc2: new GameObject({
+                x: 10,
+                y: 4,
+            }),
+        }
+    },
 }
