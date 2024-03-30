202b24356e462ac5413bc96ac282a73c914ca94b
atl97853 <128220069+atl97853@users.noreply.github.com>
2024-03-29 21:02:12 -0600

    Refactor variables, cloneBlockCode and appendTodoElementDOM functions

--- 03_to_do_app/script.js
+++ 03_to_do_app/script.js
@@ -1,45 +1,76 @@
-// variables 
-let listElementsArray = [] // i'm going to use this to store the new todo elements created 
-const firstElement = document.querySelector('.todo-sublist')
-listElementsArray.push(firstElement)
+// variables and array
+const newTodoDoRef = document.querySelector('.todo-sublist')
+let listNewTodo = []
 
+// buttons
+const createElement = document.querySelector('.create-element') // add button
+const removelement = document.querySelector('.remove-element') // remove button
+
+listNewTodo.push(newTodoDoRef)
+console.log(listNewTodo) //just for testing
+
+// const firstElement = document.querySelector('.todo-sublist')
+// listElementsArray.push(firstElement) // there is a bug here, afte deleting all the elements in the array i can't create a new "todo element"
+// // element selection
+// let todoSelected = false
+
+// functions 
 
 function cloneBlockCode() {
-    const node = document.querySelector('.todo-sublist') // this is the element that I'm going to clone
-    const clone = node.cloneNode(true)
-    console.log(clone)
-    return clone
+    const cloneTodoElement = newTodoDoRef.cloneNode(true)
+    console.log('a new todo element was cloned', cloneTodoElement) //just for testing
+    return cloneTodoElement
 }
-function createList() {
-    const testParent = document.querySelector('.todo-sublist') // the clone element is goint to be created below this parent element
-    const newDiv = cloneBlockCode() // this is the element that i'm going to "append"
-    newDiv.style.background = 'white'
+function appendTodoElementDOM() {
+    const parentElement = newTodoDoRef
+    const newCloneTodoElement = cloneBlockCode() // this is the element that i'm going to "append"
+    newCloneTodoElement.style.background = 'white'
     // insert element in the DOM
-    let parentElement = testParent.parentNode
-    parentElement.insertBefore(newDiv, testParent.nextSibling)
+    let parentElementPosition = parentElement.parentNode
+    parentElementPosition.insertBefore(newCloneTodoElement, parentElement.nextSibling)
     // append the object to an array(list)
-    listElementsArray.push(newDiv)
+    listNewTodo.push(newCloneTodoElement)
+    console.log(listNewTodo)
 }
 
-const createElement = document.querySelector('.create-element')
+// eventlisteners
+
 createElement.addEventListener('click', () => {
-    createList()
+    appendTodoElementDOM()
+    console.log('a new todo element was appended into the DOM')
 })
 
-const removelement = document.querySelector('.remove-element')
 removelement.addEventListener('click', () => {
-    listElementsArray.forEach((e) => {
-        e.remove()
+    listNewTodo.forEach(todoElement => {
+        todoElement.remove()
+        // listNewTodo.pop(todoElement)
+        console.log(listNewTodo)
     })
+    // if (todoSelected === true) {
+    //     e.remove()
+    //     todoSelected = false
+    //     console.log(todoSelected)
+    // }
 })
 
+// // maybe get the index of the element selected, and with that index delete the item
+// const mainContainer = document.querySelector('.main-container')
+// mainContainer.addEventListener('click', () => {
+//     listElementsArray.forEach((element) => {
+//         element.addEventListener('click', () => {
+//             element.style.background = 'black'
+//             element.style.color = 'white'
+//             todoSelected = true
+//             console.log(todoSelected)
+//             // let text = prompt('write something: ')
+//             // element.textContent = text
+//         })
+//     })
+// })
 
-const mainContainer = document.querySelector('.main-container')
-mainContainer.addEventListener('click', () => {
-    listElementsArray.forEach((element) => {
-        element.addEventListener('click', () => {
-            let text = prompt('write something: ')
-            element.textContent = text
-        })
-    })
-})
+
+// ---- element selection
+// listNewTodo.forEach((todoElement) => {
+//     todoElement.addEventListener('click', () => {
+//         listNewTodo = []
+//     })
