1331630ec580c6c5396f49044aac9ef1c6c773b6
atl97853 <128220069+atl97853@users.noreply.github.com>
2024-03-30 00:51:03 -0600

    Add date

--- 03_to_do_app/script.js
+++ 03_to_do_app/script.js
@@ -1,3 +1,16 @@
+// date 
+const dateContainer = document.querySelector('.date-container')
+const date = new Date()
+
+const day = date.getDate()
+const month = date.getMonth() + 1
+const year = date.getFullYear()
+
+let currentDate = `${day}/${month}/${year}`;
+dateContainer.textContent = currentDate
+console.log(currentDate)
+
+
 // variables and array
 const mainContainer = document.querySelector('.main-container')
 const newTodoDoRef = document.querySelector('.todo-element')
